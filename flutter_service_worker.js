3d001462fc1a8427d8d6bba12519481d15f18672
Mohamed Walaa Eldeen <mohamedwalaa1999@gmail.com>
2023-10-19 21:35:45 +0300

    licesned states

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -445,9 +445,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "bf0faf0913ee94caa485b8136c11d1c7
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "98b0cd923fac8dd3149c1554c17755ab",
-"/": "98b0cd923fac8dd3149c1554c17755ab",
-"main.dart.js": "ceab230b096c3e73e47dc7cc51ba1c37",
+"index.html": "9f2458fd5355d8afbd86becd800c6616",
+"/": "9f2458fd5355d8afbd86becd800c6616",
+"main.dart.js": "3463258449fc50fda291dd9991b20589",
 "manifest.json": "1094348e128ca543cea16cfb4cafe293",
 "version.json": "00e5ad2f6411328e026fb2a28b2e842d"};
 // The application shell files that are downloaded before a service worker can
