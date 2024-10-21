c21e5c812c9dd956358aa2d78bd758a31cae6bcc
Mohamed Walaa Eldeen <mohamedwalaa1999@gmail.com>
2024-10-22 00:45:55 +0300

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -476,8 +476,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "a2d66827c7e0a16011eaf71edf7ff541
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2baad8c4bf25dba3f11ebf94b2b9461c",
-"/": "2baad8c4bf25dba3f11ebf94b2b9461c",
+"index.html": "cf9685cbbad73933296d510f6b6ffbb8",
+"/": "cf9685cbbad73933296d510f6b6ffbb8",
 "main.dart.js": "e25150b0c7453bea75d159dce94253c3",
 "manifest.json": "1094348e128ca543cea16cfb4cafe293",
 "version.json": "00e5ad2f6411328e026fb2a28b2e842d"};
